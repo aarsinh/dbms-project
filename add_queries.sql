d8cb7f7ef0fadd89d55dfb1396bb091493690426
aarsinh <148481261+aarsinh@users.noreply.github.com>
2025-04-19 19:02:28 +0530

    finished add queries

--- add_queries.sql
+++ add_queries.sql
@@ -1,48 +1,176 @@
 -- Add a new patient
 CREATE OR REPLACE PROCEDURE add_patient (
-    p_aadharID IN VARCHAR,
-    p_name IN VARCHAR,
+    p_aadharID IN VARCHAR2,
+    p_name IN VARCHAR2,
     p_age IN INTEGER,
-    p_address IN VARCHAR,
-    p_primary_physician IN VARCHAR
+    p_address IN VARCHAR2,
+    p_primary_physician IN VARCHAR2
 ) AS
 BEGIN
-    INSERT INTO patients (aadharID, name, age, address, primary_physician)
+    INSERT INTO patients(aadharID, pname, age, home_address, primary_physician)
     VALUES (p_aadharID, p_name, p_age, p_address, p_primary_physician);
+
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error adding new patient: ' || SQLERRM);
 END;
+/
 
 -- Add a new pharmacy
 CREATE OR REPLACE PROCEDURE add_pharmacy (
-    p_phname IN VARCHAR,
-    p_address IN VARCHAR,
-    p_phone IN VARCHAR
+    p_phname IN VARCHAR2,
+    p_address IN VARCHAR2,
+    p_phone IN VARCHAR2
 ) AS 
 BEGIN
     INSERT INTO pharmacies (phname, address, phone)
     VALUES (p_phname, p_address, p_phone);
-END;
 
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error adding new pharma: ' || SQLERRM);
+END;
+/
+ 
 -- Add a new doctor
 CREATE OR REPLACE PROCEDURE add_doctor(
-    p_aadharID IN VARCHAR,
-    p_dname IN VARCHAR,
-    p_speciality IN VARCHAR,
+    p_aadharID IN VARCHAR2,
+    p_dname IN VARCHAR2,
+    p_speciality IN VARCHAR2,
     p_yrs_exp IN INTEGER
 ) AS 
 BEGIN 
     INSERT INTO doctors (aadharID, dname, speciality, yrs_exp) 
     VALUES (p_aadharID, p_dname, p_speciality, p_yrs_exp);
-END;
 
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error adding new doctor: ' || SQLERRM);
+END;
+/
+ 
 -- Add a new pharma company
 CREATE OR REPLACE PROCEDURE add_pharma_company(
-    p_pcname IN VARCHAR,
-    p_phone IN VARCHAR
+    p_pcname IN VARCHAR2,
+    p_phone IN VARCHAR2
 ) AS
 BEGIN
     INSERT INTO pharma_companies (pcname, phone)
     VALUES (p_pcname, p_phone);
+
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error adding new pharma company: ' || SQLERRM);
 END;
+/
 
+-- Add a new drug
+CREATE OR REPLACE PROCEDURE add_drug(
+    p_trdname IN VARCHAR2,
+    p_pcname IN VARCHAR2,
+    p_formula IN VARCHAR2
+) AS
+BEGIN
+    INSERT INTO drugs(trdname, pcname, formula) 
+    VALUES (p_trdname, p_pcname, p_formula);
 
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error adding new drug: ' || SQLERRM);
+END;
+/
 
+-- Add a new consultation
+CREATE OR REPLACE PROCEDURE add_consultation(
+    p_doctorID IN VARCHAR2,
+    p_patientID IN VARCHAR2
+) AS 
+BEGIN 
+    INSERT INTO consultations (doctorID, patientID)
+    VALUES (p_doctorID, p_patientID);
+
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error adding consultation: ' || SQLERRM);
+END;
+/
+
+-- Add a new prescription
+CREATE OR REPLACE PROCEDURE add_prescription(
+    p_doctorID IN VARCHAR2,
+    p_patientID IN VARCHAR2,
+    p_drug_name IN VARCHAR2,
+    p_pcname IN VARCHAR2,
+    p_date IN DATE,
+    p_qty IN INTEGER
+) AS
+    pres_count INTEGER;
+    existing_date DATE;     
+BEGIN
+    SELECT COUNT(*) INTO pres_count FROM prescriptions WHERE doctorID = p_doctorID AND patientID = p_patientID;
+    IF pres_count = 0 THEN
+        INSERT INTO prescriptions (doctorID, patientID, drug_name, pcname, prescription_date, quantity)
+        VALUES (p_doctorID, p_patientID, p_drug_name, p_pcname, p_date, p_qty);
+    ELSE  
+        SELECT prescription_date INTO existing_date FROM prescriptions
+        WHERE doctorID = p_doctorID AND patientID = p_patientID;    
+
+        IF p_date > existing_date THEN
+            UPDATE prescriptions
+            SET prescription_date = p_date,
+                drug_name = p_drug_name,
+                pcname = p_pcname,
+                quantity = p_qty;
+        END IF;
+    END IF;
+
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error inserting prescription: ' || SQLERRM);
+END;
+/
+
+-- Add a new drug sale
+CREATE OR REPLACE PROCEDURE add_drug_sale(
+    p_phname IN VARCHAR2,
+    p_drug_name IN VARCHAR2,
+    p_pcname IN VARCHAR2,
+    p_drug_price IN DECIMAL
+) AS
+BEGIN
+    INSERT INTO drug_sales(phname, drug_name, pcname, drug_price) 
+    VALUES (p_phname, p_drug_name, p_pcname, p_drug_price);
+
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error creating drug sale: ' || SQLERRM);
+END;
+/
+
+-- Add a new contract
+CREATE OR REPLACE PROCEDURE add_contract(
+    p_pcname VARCHAR2,
+    p_phname VARCHAR2,
+    p_start_date DATE,
+    p_end_date DATE,
+    p_content VARCHAR2,
+    p_supervisorID VARCHAR2
+) AS
+BEGIN 
+    INSERT INTO contracts (pcname, phname, start_date, end_date, content, supervisorID)
+    VALUES (p_pcname, p_phname, p_start_date, p_end_date, p_content, p_supervisorID);
+
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error creating contract: ' || SQLERRM);
+END;
+/
--- init_tables.sql
+++ init_tables.sql
@@ -1,47 +1,48 @@
--- Create patient table
-CREATE TABLE patients (
-    aadharID VARCHAR(12) PRIMARY KEY NOT NULL,
-    pname VARCHAR(50) NOT NULL,
-    age INTEGER NOT NULL,
-    address VARCHAR(50) NOT NULL,
-    primary_physician VARCHAR(12) NOT NULL,
-    FOREIGN KEY(primary_physician) REFERENCES doctors(aadharID)
-);
-
 -- Create doctors table
 CREATE TABLE doctors (
-    aadharID VARCHAR(12) PRIMARY KEY NOT NULL,
-    dname VARCHAR(50) NOT NULL,
-    speciality VARCHAR(50) NOT NULL,
+    aadharID VARCHAR2(12) PRIMARY KEY NOT NULL,
+    dname VARCHAR2(50) NOT NULL,
+    speciality VARCHAR2(50) NOT NULL,
     yrs_exp INTEGER NOT NULL
 );
 
--- Create drug table
-CREATE TABLE drugs (
-    trdname VARCHAR(50) NOT NULL,
-    pcname VARCHAR(50) NOT NULL,
-    formula VARCHAR(50) NOT NULL,
-    PRIMARY KEY(trdname, pcname),
-    FOREIGN KEY(pcname) REFERENCES pharma_companies(pcname)
+
+-- Create patient table
+CREATE TABLE patients (
+    aadharID VARCHAR2(12) PRIMARY KEY NOT NULL,
+    pname VARCHAR2(50) NOT NULL,
+    age INTEGER NOT NULL,
+    home_address VARCHAR2(50) NOT NULL,
+    primary_physician VARCHAR2(12) NOT NULL,
+    FOREIGN KEY(primary_physician) REFERENCES doctors(aadharID)
 );
 
 -- Create pharmacies table
 CREATE TABLE pharmacies (
-    phname VARCHAR(50) PRIMARY KEY NOT NULL,
-    address VARCHAR(50) NOT NULL,
-    phone VARCHAR(50) NOT NULL
+    phname VARCHAR2(50) PRIMARY KEY NOT NULL,
+    address VARCHAR2(50) NOT NULL,
+    phone VARCHAR2(50) NOT NULL
 );
 
 -- Create pharma companies table
 CREATE TABLE pharma_companies (
-    pcname VARCHAR(50) PRIMARY KEY NOT NULL,
-    phone VARCHAR(50) NOT NULL
+    pcname VARCHAR2(50) PRIMARY KEY NOT NULL,
+    phone VARCHAR2(50) NOT NULL
+);
+
+-- Create drug table
+CREATE TABLE drugs (
+    trdname VARCHAR2(50) NOT NULL,
+    pcname VARCHAR2(50) NOT NULL,
+    formula VARCHAR2(50) NOT NULL,
+    PRIMARY KEY(trdname, pcname),
+    FOREIGN KEY(pcname) REFERENCES pharma_companies(pcname)
 );
 
 -- Create consults table(relship between patient and doctor)
 CREATE TABLE consultations (
-    doctorID VARCHAR(12) NOT NULL,
-    patientID VARCHAR(12) NOT NULL,
+    doctorID VARCHAR2(12) NOT NULL,
+    patientID VARCHAR2(12) NOT NULL,
     PRIMARY KEY(doctorID, patientID),
     FOREIGN KEY(doctorID) REFERENCES doctors(aadharID),
     FOREIGN KEY(patientID) REFERENCES patients(aadharID)
@@ -49,11 +50,11 @@ CREATE TABLE consultations (
 
 -- Create prescriptions table(relship between patient, drug, doctor)
 CREATE TABLE prescriptions (
-    doctorID VARCHAR(12) NOT NULL,
-    patientID VARCHAR(12) NOT NULL,
-    drug_name VARCHAR(50) NOT NULL,
-    pcname VARCHAR(50) NOT NULL,
-    prescription_date DATETIME NOT NULL,
+    doctorID VARCHAR2(12) NOT NULL,
+    patientID VARCHAR2(12) NOT NULL,
+    drug_name VARCHAR2(50) NOT NULL,
+    pcname VARCHAR2(50) NOT NULL,
+    prescription_date DATE NOT NULL,
     quantity INTEGER NOT NULL, 
     PRIMARY KEY(doctorID, patientID, drug_name, pcname),
     FOREIGN KEY(doctorID) REFERENCES doctors(aadharID),
@@ -63,9 +64,9 @@ CREATE TABLE prescriptions (
 
 -- Create drug sales table(relationship between pharma and drug)
 CREATE TABLE drug_sales (
-    phname VARCHAR(50) NOT NULL,
-    drug_name VARCHAR(50) NOT NULL,
-    pcname VARCHAR(50) NOT NULL,
+    phname VARCHAR2(50) NOT NULL,
+    drug_name VARCHAR2(50) NOT NULL,
+    pcname VARCHAR2(50) NOT NULL,
     drug_price DECIMAL(10, 2) NOT NULL,
     PRIMARY KEY(phname, drug_name, pcname),
     FOREIGN KEY (phname) REFERENCES pharmacies(phname),
@@ -74,12 +75,12 @@ CREATE TABLE drug_sales (
 
 -- Create contracts table(relship between pc and pharma)
 CREATE TABLE contracts (
-    pcname VARCHAR(50) NOT NULL,
-    phname VARCHAR(50) NOT NULL,
-    start_date DATETIME NOT NULL,
-    end_date DATETIME NOT NULL,
-    content VARCHAR(65535) NOT NULL,
-    supervisorID VARCHAR(50) NOT NULL,
+    pcname VARCHAR2(50) NOT NULL,
+    phname VARCHAR2(50) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    content VARCHAR2(1000) NOT NULL,
+    supervisorID VARCHAR2(50) NOT NULL,
     PRIMARY KEY (pcname, phname),
     FOREIGN KEY(pcname) REFERENCES pharma_companies(pcname),
     FOREIGN KEY (phname) REFERENCES pharmacies(phname)
